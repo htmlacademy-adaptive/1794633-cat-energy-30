1d697353c09a9e988986a1511f760c11fa3afc00
belovdeveloper <belovdeveloper@gmail.com>
2024-01-10 17:19:02 +0300

    Начата адаптивная верстка крупных блоков

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,5 @@
 /* в этот файл добавляет скрипты*/
+document.querySelector('.main-header__menu-toggle').addEventListener('click', ()=> {
+  document.querySelector('.main-header__menu').classList.toggle('main-header__menu--closed');
+  document.querySelector('.main-header__menu-toggle').classList.toggle('main-header__menu-toggle--closed');
+});
