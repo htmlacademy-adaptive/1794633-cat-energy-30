b0c8165badebb65015d5aaa01c88845929300b86
belovdeveloper <belovdeveloper@gmail.com>
2024-01-29 17:51:02 +0300

    Добавлена обработка состояния no-js. Исправлена неработающая кнопка в секции Hero.

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,4 +1,4 @@
-/* в этот файл добавляет скрипты*/
+document.querySelector('.main-header__menu').classList.remove('main-header__menu--no-js');
 document.querySelector('.main-header__menu-toggle').addEventListener('click', ()=> {
   document.querySelector('.main-header__menu').classList.toggle('main-header__menu--closed');
   document.querySelector('.main-header__menu-toggle').classList.toggle('main-header__menu-toggle--closed');
